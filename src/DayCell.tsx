1cc9c545aba3ebf6f2bbae3d4aaa986f84e937d5
catigator <jakob.peeker@gmail.com>
2023-11-28 15:48:03 +0000

    Added canOpen and cannotOpen classes

--- src/DayCell.tsx
+++ src/DayCell.tsx
@@ -11,10 +11,17 @@ const DayCell = (props: CellProps) => {
   let onclick = (event: any) => {
       console.log("clicked!   " + props.day);
   }
+
+  const canOpen = 5 - props.day > 0;
+
+  let className = "day-cell cannotOpen"
+  if (canOpen) {
+    className = "day-cell canOpen"
+  }
   
   return(
     <React.Fragment>
-      <li className={"day-cell"} id={String(props.day)} onClick={onclick}>
+      <li className={className} id={String(props.day)} onClick={onclick}>
         <div className="day-cell-text">{props.day}</div>
         </li>
     </React.Fragment>
