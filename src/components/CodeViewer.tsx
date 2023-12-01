2558aa0e6e93c1e627312d2853e0c15f2a64dd7c
catigator <jakob.peeker@gmail.com>
2023-12-01 13:10:57 +0000

    Fixed code viewer css

--- src/components/CodeViewer.tsx
+++ src/components/CodeViewer.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import '../css/CodeViewer.scss';
 import Prism from "prismjs";
 import "../css/prism.css";
 import 'prismjs/components/prism-python'; // Language
 // import "prismjs/themes/prism-tomorrow.css";
 import "../css/prism-material-light.scss";
+import publicBase from "/?url"
 
 
 let Code = function (code: string, language: string) {
@@ -24,12 +25,31 @@ let Code = function (code: string, language: string) {
 
 const CodeViewer = () => {
 
+  let [codeFileString, setCodeFileString] = useState("");
+
+  async function fetchHtml(url: string) {
+    const codeString = await (await fetch(url)).text();
+    console.log(codeString);
+    setCodeFileString(codeString);
+    console.log(codeFileString);
+  }
+
+  useEffect(() => {
+    fetchHtml( publicBase + "aoc_2023/aoc/day_01/__main__.py");
+  }, []);
+  
+  useEffect(() => {
+    Prism.highlightAll();
+  }, [codeFileString]);
+
+
   let generateCodeViewer = () => {
 
-    const code = `def some_function():
-        a = 4 + 3
-        return a
-        `
+    // const code = `def some_function():
+    //     a = 4 + 3
+    //     return a
+    //     `
+    fetchHtml( publicBase + "aoc_2023/aoc/day_01/__main__.py");
 
     const codeViewer = (
       <div className="code-viewer-container"> 
@@ -37,7 +57,9 @@ const CodeViewer = () => {
         <div className="code-viewer-header">
           Code
         </div>
-          {Code(code, "python")}
+        <div className="code-container">
+          {Code(codeFileString, "python")}
+          </div>
         </div>
       </div>
     );
@@ -46,10 +68,6 @@ const CodeViewer = () => {
   }
 
   const reactCodeViewer = generateCodeViewer();
-
-  useEffect(() => {
-    Prism.highlightAll();
-  }, []);
 
 
 
