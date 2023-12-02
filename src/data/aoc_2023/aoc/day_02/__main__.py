b8a3015581e7ccaac80793d7d82edf2cae918660
Jakob Pettersson Peeker <jakob.peeker@eigentech.com>
2023-12-02 10:03:06 +0000

    Solved Day 2 Part 2

--- src/data/aoc_2023/aoc/day_02/__main__.py
+++ src/data/aoc_2023/aoc/day_02/__main__.py
@@ -8,7 +8,8 @@ from utils.helper_functions import (
     read_input_int_matrix
 )
 from dataclasses import dataclass
-from typing import List
+from typing import List, Dict
+
 
 
 INPUT_FILENAME = "aoc/day_02/INPUT.txt"
@@ -22,7 +23,7 @@ TOTALS = {
 
 
 @dataclass
-class ColorNumberTuple:
+class ColorShow:
     number: int
     color: str
     game: int
@@ -31,15 +32,15 @@ class ColorNumberTuple:
 @dataclass
 class Game:
     id: int
-    color_number_tuples: List[ColorNumberTuple]
+    color_shows: List[ColorShow]
 
 
-def get_color_number_tuples(line):
+def get_color_shows(line):
     split_line = line.split(": ")
     game = int(split_line[0][5:])
     shows_str = split_line[1].split("; ")
 
-    color_number_tuples: List[ColorNumberTuple] = []
+    color_shows: List[ColorShow] = []
 
     for show_str in shows_str:
         split_show = show_str.split(", ")
@@ -47,27 +48,45 @@ def get_color_number_tuples(line):
             split_tuple_str = tuple_str.split(" ")
             number = int(split_tuple_str[0])
             color = split_tuple_str[1]
-            color_number_tuples.append(ColorNumberTuple(number=number, color=color, game=game))
+            color_shows.append(ColorShow(number=number, color=color, game=game))
 
-    return color_number_tuples
+    return color_shows
 
 
 def get_game(line) -> Game:
-    color_number_tuples = get_color_number_tuples(line)
-    game = Game(color_number_tuples=color_number_tuples, id=color_number_tuples[0].game)
+    color_shows = get_color_shows(line)
+    game = Game(color_shows=color_shows, id=color_shows[0].game)
     return game
 
 
-def is_possible(cnp: ColorNumberTuple):
+def is_possible(cnp: ColorShow):
     return cnp.number <= TOTALS[cnp.color]
 
 
 def is_game_possible(game: Game):
-    for tup in game.color_number_tuples:
+    for tup in game.color_shows:
         if not is_possible(tup):
             return False
 
     return True
+
+
+def get_fewest_colors_possible_for_game(game: Game) -> Dict[str, int]:
+    fewest_colors = {
+        "red": 0,
+        "green": 0,
+        "blue": 0,
+    }
+    for color_show in game.color_shows:
+        if color_show.number > fewest_colors[color_show.color]:
+            fewest_colors[color_show.color] = color_show.number
+
+    return fewest_colors
+
+
+def get_power(colors: Dict[str, int]):
+    power = colors["red"] * colors["green"] * colors["blue"]
+    return power
 
 
 @time_it
@@ -86,7 +105,18 @@ def solve_part_1():
 def solve_part_2():
     print("Day 02 - Part 2")
 
+    lines = read_input_lines_strip_newline(INPUT_FILENAME)
+    power_sums = []
+
+    for line in lines:
+        game = get_game(line)
+        fewest_colors = get_fewest_colors_possible_for_game(game)
+        power = get_power(fewest_colors)
+        power_sums.append(power)
+
+    print(f"The sums of all powers is {sum(power_sums)}")
+
 
 if __name__ == "__main__":
     solve_part_1()
-    # solve_part_2()
+    solve_part_2()
