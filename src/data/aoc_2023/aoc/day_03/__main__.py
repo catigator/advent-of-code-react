8c95cc15e6cc28c5bc4a1a510935f6ca683a93ba
Jakob Pettersson Peeker <jakob.peeker@eigentech.com>
2023-12-04 17:09:14 +0000

    Solved Day 3 Part 3

--- src/data/aoc_2023/aoc/day_03/__main__.py
+++ src/data/aoc_2023/aoc/day_03/__main__.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from utils.decorators import time_it
 from utils.helper_functions import (
     read_input,
@@ -7,8 +9,9 @@ from utils.helper_functions import (
     read_input_int_matrix
 )
 from dataclasses import dataclass
-from typing import List
+from typing import List, Optional
 import re
+import math
 
 
 INPUT_FILENAME = "aoc/day_03/INPUT.txt"
@@ -25,9 +28,25 @@ class Number:
     row: int
 
 
+@dataclass
 class Point:
     row: int
     pos: int
+
+    def __str__(self):
+        return str(self.row) + "-" + str(self.pos)
+
+
+@dataclass
+class Symbol:
+    value: str
+    point: Point
+    numbers: List[Number]
+    ratio: Optional[int] = 0
+
+    def __str__(self):
+        return str(self.value) + ": " + str(self.point)
+
 
 NON_SYMBOL_CHARS = "0123456789."
 
@@ -45,13 +64,37 @@ def check_if_has_adjacent_symbol(number: Number, lines):
     return False
 
 
+def get_adjecent_gear_symbols(number: Number, lines, symbols: List[Symbol]):
+    start = max(number.start - 1, 0)
+    end = min(number.end + 1, len(lines[number.row]))
+
+    for i in range(-1, 2):
+        row = number.row + i
+        if row > -1 and row < len(lines):
+            chars = lines[row][start:end]
+            for i_char, c in enumerate(chars):
+                if c == "*":
+                    point = Point(row=row, pos=i_char + start)
+                    if str(point) in symbols:
+                        symbols[str(point)].numbers.append(number)
+                    else:
+                        symbol = Symbol(
+                            value="*",
+                            point=point,
+                            numbers=[number],
+                        )
+                        symbols[str(point)] = symbol
+
+    return symbols
+
+
 def get_numbers_for_line(line: str, row: int) -> List[Number]:
     numbers = []
     number_matches = list(re.finditer("\d+", line))
     for number_match in number_matches:
         numbers.append(
             Number(
-                value=number_match.group(),
+                value=int(number_match.group()),
                 start=number_match.regs[0][0],
                 end=number_match.regs[0][1],
                 row=row,
@@ -81,8 +124,25 @@ def solve_part_1():
 @time_it
 def solve_part_2():
     print("Day 03 - Part 2")
+    lines = read_input_lines(INPUT_FILENAME, strip_whitespace=True)
+    part_numbers = []
+
+    symbols = {}
+
+    for i, line in enumerate(lines):
+        numbers = get_numbers_for_line(line, i)
+        if numbers is not None:
+            for number in numbers:
+                get_adjecent_gear_symbols(number, lines, symbols)
+
+    gear_symbols = [symbol for symbol in symbols.values() if len(symbol.numbers) > 1]
+
+    for gear_symbol in gear_symbols:
+        gear_symbol.ratio = math.prod(number.value for number in gear_symbol.numbers)
+
+    print(f"The sum of all ratios is {sum(gear_symbol.ratio for gear_symbol in gear_symbols)}")
 
 
 if __name__ == "__main__":
-    solve_part_1()
-    # solve_part_2()
+    # solve_part_1()
+    solve_part_2()
