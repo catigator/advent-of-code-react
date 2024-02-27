6746830a3264595958e4e37af1edc218ce59f62d
Jakob Pettersson Peeker <jakob.peeker@eigentech.com>
2024-02-27 12:47:32 +0100

    Dynamic Puzzle Day URL

--- src/components/PuzzleViewer.tsx
+++ src/components/PuzzleViewer.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import '../css/PuzzleViewer.scss';
 import '../css/scrollbar.scss';
+import { useAtom } from 'jotai';
+import { DayAtom } from "../components/DayAtom";
 // import day1HtmlString from "../data/aoc_2023/aoc/day_01/PUZZLE.html?raw";
 // import day1Html from "data/aoc_2023/aoc/day_01/PUZZLE.html?url";
 // import pythonExample from "/aoc_2023/aoc/day_01/__main__.py?url"; // works!!!
@@ -10,6 +12,7 @@ import '../css/scrollbar.scss';
 const PuzzleViewer = () => {
 
   // let [htmlFileString, setHtmlFileString] = useState("");
+  const [day, setDay]: any = useAtom<number>(DayAtom);
 
   // async function fetchHtml(url: string) {
   //   const htmlString = await (await fetch(url)).text();
@@ -25,9 +28,10 @@ const PuzzleViewer = () => {
   //   fetchHtml(day1Html); // works!
   // }, []);
 
-  let generatePuzzleViewer = () => {
+  let generatePuzzleViewer = (puzzleDay: Number) => {
 
     // fetchHtml();
+    let adventOfCodePuzzleUrl = "https://adventofcode.com/2023/day/" + puzzleDay;
 
     const puzzleViewer = (
       <div className="puzzle-viewer-component">
@@ -37,7 +41,7 @@ const PuzzleViewer = () => {
         {/* <div className="puzzle-viewer container-problem" dangerouslySetInnerHTML={{ __html: day1HtmlString }}>
         </div> */}
         <div className="puzzle-viewer container-problem">
-          See puzzle at <a href="https://adventofcode.com/2023/day/1" target="_blank">https://adventofcode.com/2023/day/1</a>
+          See puzzle at <a href={adventOfCodePuzzleUrl} target="_blank">{adventOfCodePuzzleUrl}</a>
         </div>
       </div>
     )
@@ -45,7 +49,7 @@ const PuzzleViewer = () => {
     return puzzleViewer;
   }
 
-  let puzzleViewer = generatePuzzleViewer();
+  let puzzleViewer = generatePuzzleViewer(day);
 
   return (
     <React.Fragment>
